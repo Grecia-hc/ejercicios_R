09d538f9882fa7af7a2a02e2ba27a0a9abc0bf45
Grecia Hernandez <grematc@gmail.com>
2021-09-24 10:24:41 -0500

    ej_3

---
+++ 2_R.R
@@ -0,0 +1,6 @@
+#ejercicio_2
+
+m <- matrix(1:12,3,4)
+#Ejercicio matriz
+x <- 15
+
---
+++ ej_1.R
@@ -0,0 +1,13 @@
+##primer script
+#A continuación cargaré el paquete ggplot2
+getwd()
+#a demás cargaré el paquete ggplot2
+library(ggplot2)
+sessionInfo()
+
+#A continuación definiré algunos objetos dentro de mi sesión de R
+x<-3
+y<-4
+z<-5
+w<-10
+
